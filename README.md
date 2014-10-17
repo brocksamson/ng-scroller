e2e6d790bd335c1d7e5839051871aeaec4178f66
Matthew Miller <matthewmillerinc@gmail.com>
2014-10-17 15:00:39 -0500

    Initial npm installs and initial gulp file

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ ng-scroller
 ===========
 
 An infinite scroller for angularjs
+
+Getting Started
+=============
+
+install npm
+npm install -g gulp
+npm install
---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
+var livereload = require('gulp-livereload');
+var watch = require('gulp-watch');
+var jshint = require('gulp-jshint');
+var del = require('del');
+
+gulp.task('default', ['build', 'test'], function() {});
+
+gulp.task('clean', function(cb){
+    del(['dist/*'], cb);
+});
+
+gulp.task('build', ['clean'], function(){
+    return gulp.src(['src/*.js'])
+        .pipe(jshint())
+        .pipe(ngAnnotate())
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('test', function(){
+    return gulp.src(['test/test-*.js'], { read: false })
+        .pipe(mocha({
+            reporter: 'spec',
+            globals: {
+                should: require('should')
+            }
+        }));
+});
+
+gulp.task('watch', function(){
+    livereload.listen();
+    gulp.watch(['test/*.js', 'src/*.js'], ['build', 'test'])
+    .on('change', livereload.changed);
+});
