0ba280254b8f63de56866affd508b309f05c2d04
Matthew Miller <matthewmillerinc@gmail.com>
2014-10-17 15:41:07 -0500

    Added angular to npm build

---
+++ test/test-scroller.js
@@ -0,0 +1,7 @@
+describe('ng-scroller', function(){
+    describe('validation', function(){
+        it('should require a properly formatted datasource directive', function(){
+
+        });
+    });
+})
