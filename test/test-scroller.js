f0312f86adf1380de6b1f747ed63ee8199ce4914
Matthew Miller <matthewmillerinc@gmail.com>
2014-10-18 13:06:57 -0500

    Angularjs pulled into mocha

--- test/test-scroller.js
+++ test/test-scroller.js
@@ -1,7 +1,9 @@
 describe('ng-scroller', function(){
     describe('validation', function(){
         it('should require a properly formatted datasource directive', function(){
-
+            inject(function($rootScope){
+                var scope = $rootScope.$new();
+            });
         });
     });
 })
